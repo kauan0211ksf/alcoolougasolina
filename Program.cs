779d9eb90bfe5701b52483a922d1f5c412039714
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-29 22:30:57 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+Console.WriteLine("-- Alcool Ou Gasolina --\n");
+
+    Console.Write("Digite o valor do etanol: ");
+double valorEtanol = double.Parse(Console.ReadLine()!);
+
+    Console.Write("Digite o valor da gasolina: ");
+double valorGasolina = double.Parse(Console.ReadLine()!);
+
+double razaoPrecos = CalcularRazaoPrecos(valorEtanol, valorGasolina);
+double razaoPrecosPorcentagem = razaoPrecos * 100;
+    Console.WriteLine($"Razão entre os preços: {razaoPrecosPorcentagem:N1}%");
+
+bool abastecerComGasolina = VerificarRecomendacao(razaoPrecos);
+    Console.WriteLine($"Recomendação: Abastecer com {((abastecerComGasolina) ? "Gasolina" : "Etanol")}");
+
+static double CalcularRazaoPrecos(double valorEtanol, double valorGasolina) {
+return valorEtanol / valorGasolina;
+}
+
+static bool VerificarRecomendacao(double razaoPrecos) {
+double percentualIdeal = 0.73;
+return razaoPrecos > percentualIdeal;
+}
+
